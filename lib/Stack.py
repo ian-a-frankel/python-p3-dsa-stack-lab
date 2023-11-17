4bdd45f2e061283313d7d468390440f4b80eaf59
Ian Frankel <ian.frankel@gmail.com>
2023-11-17 10:40:04 -0500

    stacked up

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[len(self.items) - 1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
-    def search(self, target):
-        pass
+    def search(self, value):
+        n = 1
+        while n < len(self.items) + 1:
+            if self.items[len(self.items) - n] == value:
+                return n - 1
+            n += 1
+        return -1
